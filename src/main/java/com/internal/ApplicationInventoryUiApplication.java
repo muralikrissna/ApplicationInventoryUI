778fc70f9b80cb4651538e1873a92cf50507b4ba
muralikrissna <muralikrishna.dasireddy@gmail.com>
2021-03-29 16:46:18 +0530

    Initial Commit

---
+++ src/main/java/com/internal/ApplicationInventoryUiApplication.java
@@ -0,0 +1,13 @@
+package com.internal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationInventoryUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationInventoryUiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/internal/ApplicationInventoryUiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.internal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationInventoryUiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
